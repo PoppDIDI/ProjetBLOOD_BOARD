3d0e16340d0bd368853235d9fccb62dcfe4b03b2
JMahatoky <mahatoky04@gmail.com>
2023-11-01 11:32:04 +0300

    Modification lien Database Helpers

--- ProjetBLOOD_BOARD/Helpers/DatabaseHelper.cs
+++ ProjetBLOOD_BOARD/Helpers/DatabaseHelper.cs
@@ -15,7 +15,7 @@ namespace ProjetBLOOD_BOARD.Helpers
 {
     public static class DatabaseHelper
     {
-        private static readonly string ConnectionString = "Data Source=DESKTOP-FAKAJVL\\SQLEXPRESS;Initial Catalog = testemaivana; Integrated Security = True;";
+        private static readonly string ConnectionString = "Data Source=DESKTOP-9TF6LVH\\SQLEXPRESS;Initial Catalog = base de donne Blood_Board;User Id=projet;Password=projet;";
 
        /* public static void EnregistrerDonnees(string donnee1, string donnee2)
         {
